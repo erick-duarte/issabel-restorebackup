058f1a43124d2b174a6e080ae5a038a2d037466a
Erick Duarte <erkfduarte@gmail.com>
2021-12-19 20:30:52 -0300

    commit

--- restore_backup.sh
+++ restore_backup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# PT # O script aqui está bem basico, mas na verdade ele é mais complexo, pois existe algumas particularidades
+# da empresa para qual fiz isso. Caso tiver interesse só entrar em contato.
+
+# EN # The script here is very basic, but actually it is more complex, as there are some peculiarities
+# of the company I did it for. If you are interested, just get in touch.
+
 CLIENTE=$1
 FILIAL=$2
 DIRBASE="/var/www/backup/"
 
-## PT - Isso foi criado pois alguns clientes especifico tem mais de um servidor(filial)
-## EN - This was created because some specific clients have more than one server (branch)
+## PT - Isso foi criado pois alguns clientes especificoq que tem mais de um servidor(filial)
+## EN - This was created because some customers specify that it has more than one server (branch)
 LISTCLIENT='CLIENTE X'
 LISTFILIAL=('FILIAL X' \
 'FILIAL Y' \
@@ -18,36 +24,24 @@ then
         if [ "${i}" == "${FILIAL}" ]
         then
             BKPFILETAR=$(/usr/bin/rclone lsf --include "*.tar" GOOGLEDRIVE:DIRETORIOS | /usr/bin/head -n 1)
-            BKPFILESQL=$(/usr/bin/rclone lsf --include "*.sql" GOOGLEDRIVE:DIRETORIOS | /usr/bin/head -n 1)
-            FILES=( $BKPFILETAR $BKPFILESQL ) # Isso nem precisava, mas só queria deixar a coisas mais interessantes
+            FILES=( $BKPFILETAR ) # Isso nem precisava, mas só queria deixar a coisas mais interessantes
             for i in "${FILES[@]}"
             do
                 /usr/bin/rclone copy GOOGLEDRIVE:DIRETORIOS "$DIRBASE"
             done
             /usr/share/issabel/privileged/backupengine --restore --backupfile="$BKPFILETAR"
-            if [ -f $DIRBASE/ARQUIVOSQL.sql ]
-            then
-                /usr/bin/echo "create database DB;"|/usr/bin/mysql -u root -pSENHA
-                /usr/bin/mysql -u root -pSENHA DB < $DIRBASE/ARQUIVOSQL.sql
-            fi
-            /usr/bin/rm -rf $DIRBASE/{*.sql,*,tar}
+            /usr/bin/rm -rf $DIRBASE/{*,tar}
             exit 0
         fi
     done
 else
     BKPFILETAR=$(/usr/bin/rclone lsf --include "*.tar" GOOGLEDRIVE:DIRETORIOS | /usr/bin/head -n 1)
-    BKPFILESQL=$(/usr/bin/rclone lsf --include "*.sql" GOOGLEDRIVE:DIRETORIOS | /usr/bin/head -n 1)
-    FILES=( $BKPFILETAR $BKPFILESQL ) # Isso nem precisava, mas só queria deixar a coisas mais interessantes
+    FILES=( $BKPFILETAR ) # Isso nem precisava, mas só queria deixar a coisas mais interessantes
     for i in "${FILES[@]}"
     do
         /usr/bin/rclone copy GOOGLEDRIVE:DIRETORIOS "$DIRBASE"
     done
     /usr/share/issabel/privileged/backupengine --restore --backupfile="$BKPFILETAR"
-    if [ -f $DIRBASE/ura_rever*.sql ]
-    then
-        /usr/bin/echo "create database DB;"|/usr/bin/mysql -u root -pSENHA
-        /usr/bin/mysql -u root -pSENHA DB < $DIRBASE/ura_rever*.sql
-    fi
-    /usr/bin/rm -rf $DIRBASE/{*.sql,*,tar}
+    /usr/bin/rm -rf $DIRBASE/{*,tar}
     exit 0
 fi
